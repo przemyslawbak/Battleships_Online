e31bdcc4cde0024dc86c5a9ba5d325c031bc655f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-08-25 15:00:50 +0200

    Added: nav component, auth service; Updated: app component;

--- Battleships.AppWeb/AppClient/src/app/app.module.ts
+++ Battleships.AppWeb/AppClient/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent
   ],
   imports: [
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+      { path: 'login-facebook', component: LoginFacebookComponent }
+    ]),
     BrowserModule,
     AppRoutingModule
   ],
