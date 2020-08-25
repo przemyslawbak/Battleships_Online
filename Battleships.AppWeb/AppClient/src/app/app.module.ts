4d75f2d3439d6644cc0697e41d5c9fcd7db81be0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-08-25 22:46:46 +0200

    Added new components and updated auth sevice, removed compilation errors;

--- Battleships.AppWeb/AppClient/src/app/app.module.ts
+++ Battleships.AppWeb/AppClient/src/app/app.module.ts
@@ -1,26 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { AuthService } from './services/auth.service';
+import { AuthInterceptor } from './services/auth.interceptor';
+//import { LoginComponent } from './login/login.component';
+//import { RegisterComponent } from './register/register.component';
+//import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    //LoginComponent,
+    //RegisterComponent
   ],
   imports: [
+    HttpClientModule,
     RouterModule.forRoot([
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'login-facebook', component: LoginFacebookComponent }
+      //{ path: 'login', component: LoginComponent },
+      //{ path: 'register', component: RegisterComponent },
+      //{ path: 'login-facebook', component: LoginFacebookComponent }
     ]),
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
