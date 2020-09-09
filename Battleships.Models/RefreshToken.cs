af0d605d53205e0391ba46a220a6dd10133690bd
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-09-09 07:08:34 +0200

    Correcting refresh token entity bugs;

--- Battleships.Models/RefreshToken.cs
+++ Battleships.Models/RefreshToken.cs
@@ -1,7 +1,11 @@
-﻿namespace Battleships.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Battleships.Models
 {
     public class RefreshToken
     {
+        [Key]
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Token { get; set; }
         public string IpAddress { get; set; }
