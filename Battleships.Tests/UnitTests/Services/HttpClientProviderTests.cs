5462a6d620e9f72cb508a1473d8840542b5ab65a
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-09-22 21:31:23 +0200

    HttpClientProvider empty unit test file added;

---
+++ Battleships.Tests/UnitTests/Services/HttpClientProviderTests.cs
@@ -0,0 +1,16 @@
+﻿using Battleships.Services;
+
+namespace Battleships.Tests.UnitTests.Services
+{
+    public class HttpClientProviderTests
+    {
+        private readonly HttpClientProvider _service;
+
+        public HttpClientProviderTests()
+        {
+            _service = new HttpClientProvider();
+        }
+
+        //Nothing to be tested.
+    }
+}
