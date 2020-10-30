6b9ac023d937618a8f88c1aace1b55b8195bf485
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-10-30 21:06:05 +0100

    Design of game header and menu;

--- Battleships.AppWeb/AppClient/src/app/app-game/game-menu/game-menu.component.ts
+++ Battleships.AppWeb/AppClient/src/app/app-game/game-menu/game-menu.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '@services/auth.service';
+
 @Component({
   selector: 'app-game-menu',
   templateUrl: './game-menu.component.html',
   styleUrls: ['./game-menu.component.css'],
 })
-export class GameMenuComponent {}
+export class GameMenuComponent {
+  constructor(public auth: AuthService) {}
+}
