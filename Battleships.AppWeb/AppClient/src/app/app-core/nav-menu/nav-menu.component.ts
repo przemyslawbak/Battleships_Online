5293230da0ee27ad075f23830658b4aee008528f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-10-30 20:17:29 +0100

    Cleaned up nav-menu.component;

--- Battleships.AppWeb/AppClient/src/app/app-core/nav-menu/nav-menu.component.ts
+++ Battleships.AppWeb/AppClient/src/app/app-core/nav-menu/nav-menu.component.ts
@@ -11,22 +11,12 @@ export class NavMenuComponent {
   public userName: string;
   constructor(public auth: AuthService) {}
 
-  public isExpanded = false;
-
   public getUserDisplayName() {
     if (this.auth.isLoggedIn()) {
       return this.auth.getAuth().displayName ?? 'logged in user';
     } else {
       return 'guest';
     }
-  }
-
-  public collapse() {
-    this.isExpanded = false;
-  }
-
-  public toggle() {
-    this.isExpanded = !this.isExpanded;
   }
 
   public logout(): boolean {
