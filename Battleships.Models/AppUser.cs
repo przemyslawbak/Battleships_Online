82aea821a4500bffc6feaa7ee5b15c6c71ef04a6
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-01 17:14:32 +0100

    Updated user model, added view model for displaying best players;

--- Battleships.Models/AppUser.cs
+++ Battleships.Models/AppUser.cs
@@ -5,5 +5,7 @@ namespace Battleships.Models
     public class AppUser : IdentityUser
     {
         public string DisplayName { get; set; }
+        public int WonMultiplayerGames { get; set; }
+        public int WonAiGames { get; set; }
     }
 }
---
+++ Battleships.Models/ViewModels/BestPlayersViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Battleships.Models.ViewModels
+{
+    public class BestPlayersViewModel
+    {
+        public string DisplayName { get; set; }
+        public int WonAiGames { get; set; }
+        public int WonMultiplayerGames { get; set; }
+    }
+}
