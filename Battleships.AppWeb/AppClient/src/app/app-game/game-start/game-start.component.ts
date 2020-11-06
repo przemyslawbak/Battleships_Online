2bd91927d38efa12e7b57e54836766140fff6ef8
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-06 22:39:39 +0100

    Routing from game start component to game play; Updated TODO;

--- Battleships.AppWeb/AppClient/src/app/app-game/game-start/game-start.component.ts
+++ Battleships.AppWeb/AppClient/src/app/app-game/game-start/game-start.component.ts
@@ -109,6 +109,7 @@ export class GameStartComponent {
     const url = environment.apiUrl + 'api/game/start';
     this.http.post(url, model).subscribe(() => {
       this.spinner.hide();
+      this.router.navigate(['play']);
     });
   }
 }
