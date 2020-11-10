3b99006ca36fa9f92e02cd565fa5e0068b415499
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-10 21:32:08 +0100

    Updated play component routing for case where there is no game ID;

--- Battleships.AppWeb/AppClient/src/app/app-routing.module.ts
+++ Battleships.AppWeb/AppClient/src/app/app-routing.module.ts
@@ -34,6 +34,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
+    path: 'play',
+    component: GamePlayComponent,
+    canActivate: [AuthGuard],
+  },
+  {
     path: 'enter',
     component: GameEnterComponent,
     canActivate: [AuthGuard],
