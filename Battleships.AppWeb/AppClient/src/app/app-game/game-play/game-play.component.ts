25f901db03774051151127032b69765bca88b0ee
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-12 19:39:03 +0100

    Working on game joining logic in game-play component; Chnaged method names;

--- Battleships.AppWeb/AppClient/src/app/app-game/game-play/game-play.component.ts
+++ Battleships.AppWeb/AppClient/src/app/app-game/game-play/game-play.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { GameService } from '@services/game.service';
 import { SignalRService } from '@services/signal-r.service';
+import { HttpService } from '@services/http.service';
+import { environment } from '@environments/environment';
 
 @Component({
   templateUrl: './game-play.component.html',
@@ -21,18 +23,32 @@ export class GamePlayComponent implements OnInit {
     private router: Router,
     private game: GameService,
     private route: ActivatedRoute,
-    private signalRService: SignalRService
+    private signalRService: SignalRService,
+    private http: HttpService
   ) {}
 
   public ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
-      //todo: if game exists-> see if client was already playing this game id
-      //todo: if want to join-> see if no restricted
-      //todo: if game restricted-> message
-      //todo: if game is full-> message
-      //todo: if game NOT exists-> message
+      const url = environment.apiUrl + 'api/game/join?id=' + id;
+      this.http.getData(url).subscribe((game) => {
+        if (game) {
+          alert('game state OK');
+          //todo: check if player is already playing it
+          //if playing already: this.game = game; ->init
+          //if NOT playing already: check if multiplayer
+          //if NOT multiplayer: cancel-> message
+          //if multiplayer: check if free slot
+          //if NOT free slot: cancel-> message
+          //if free slot: this.game = game; -> update props ->init
+        } else {
+          alert('game state NOT OK');
+          //message: game could not be found
+        }
+      });
+      //if game slot empty join game (update game status), else message
 
+      //todo: add messages[] property to the game status model, keep limited qty of messages
       //todo: if can play:
       this.initGame();
     } else {
@@ -48,8 +64,8 @@ export class GamePlayComponent implements OnInit {
     this.initVar();
     this.drawBoards();
     this.signalRService.startConnection();
-    this.signalRService.addMessageListener();
-    this.signalRService.broadcastMessage(); //todo: remove after testing
+    this.signalRService.addGameStateListener();
+    this.signalRService.broadcastGameState();
   }
 
   public fireTorpedo(j: number, k: number) {
@@ -60,8 +76,8 @@ export class GamePlayComponent implements OnInit {
 
   private initVar(): void {
     let game = this.game.getGame();
-    this.gameStatus = game.gameStage;
-    this.whoseTurn = game.gameTurnPlayer;
+    this.gameStatus = game.gameStage.toString();
+    this.whoseTurn = game.gameTurnPlayer.toString();
     this.gameTurnNumber = game.gameTurnNumber;
     this.player1 = game.playersDisplay[0];
     this.player2 = game.playersDisplay[1];
