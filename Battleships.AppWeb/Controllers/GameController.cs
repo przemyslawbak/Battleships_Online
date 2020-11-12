162f97e61d09f0886ccb07471319284a8078bcad
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-12 19:44:03 +0100

    Updated GameController with JoinGame endpoint; StartGame endpoint adds new game to the cahced list;

--- Battleships.AppWeb/Controllers/GameController.cs
+++ Battleships.AppWeb/Controllers/GameController.cs
@@ -1,6 +1,9 @@
-﻿using Battleships.Models.ViewModels;
+﻿using Battleships.Models;
+using Battleships.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Battleships.AppWeb.Controllers
 {
@@ -8,15 +11,45 @@ namespace Battleships.AppWeb.Controllers
     [ApiController]
     public class GameController : Controller
     {
+        private readonly IMemoryAccess _memoryAccess;
+
+        public GameController(IMemoryAccess memory)
+        {
+            _memoryAccess = memory;
+        }
+
         /// <summary>
         /// GET: api/game/start
         /// </summary>
         /// <returns>Status code.</returns>
         [HttpPost("start")]
         [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin, User")]
-        public IActionResult StartGame(GameStartViewModel model)
+        public IActionResult StartGame(GameStateModel model)
         {
+            List<GameStateModel> list = _memoryAccess.GetGameList();
+            list.Add(model);
+            _memoryAccess.SetGameList(list);
+
             return Ok();
+        }
+
+        /// <summary>
+        /// GET: api/game/join
+        /// </summary>
+        /// <returns>Game status model.</returns>
+        [HttpGet("join")]
+        [Authorize(AuthenticationSchemes = "Bearer", Roles = "Admin, User")]
+        public IActionResult JoinGame(int id)
+        {
+            List<GameStateModel> list = _memoryAccess.GetGameList();
+            GameStateModel model = _memoryAccess.GetGameList().Where(g => g.GameId == id).FirstOrDefault();
+
+            if (model == null)
+            {
+                return new ObjectResult("Game does not exists.") { StatusCode = 409 };
+            }
+
+            return Json(model);
         }
     }
 }
