5c6f2139251f7d0842ec2b4f62dc8863f9c889a0
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-12 19:41:36 +0100

    Created game stage backend model;

---
+++ Battleships.Models/GameStateModel.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Battleships.Models
+{
+    public enum GameStage
+    {
+        Deploying,
+        Playing
+    }
+
+    public enum WhoseTurn
+    {
+        Player1,
+        Player2
+    }
+
+    public class GameStateModel
+    {
+        public int GameId { get; set; }
+        public int GameTurnNumber { get; set; }
+        public bool GameAi { get; set; }
+        public bool GameMulti { get; set; }
+        public bool GameOpen { get; set; }
+        public bool GameLink { get; set; }
+        public GameStage GameStage { get; set; }
+        public WhoseTurn WhoseTurn { get; set; }
+        public bool[][] Player1Fleet { get; set; }
+        public bool[][] Player2Fleet { get; set; }
+        public string[] PlayersDisplay { get; set; }
+        public string[] PlayersNames { get; set; }
+        public bool[][] BoardP1 { get; set; }
+        public bool[][] BoardP2 { get; set; }
+    }
+}
