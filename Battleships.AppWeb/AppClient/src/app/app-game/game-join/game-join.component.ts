e90a70d539f5be8f5a1c28ad370bf70e22bf2a3c
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-11-13 22:16:36 +0100

    In game-join component`s list displaying `Playing now` next to the game if user is already playing that game; Changed game join link style to the button styling; Added game service to the component and assigning game ID to the `currentGame` variable;

--- Battleships.AppWeb/AppClient/src/app/app-game/game-join/game-join.component.ts
+++ Battleships.AppWeb/AppClient/src/app/app-game/game-join/game-join.component.ts
@@ -3,6 +3,7 @@ import { environment } from '@environments/environment';
 
 import { AuthService } from '@services/auth.service';
 import { HttpService } from '@services/http.service';
+import { GameService } from '@services/game.service';
 
 import { OpenGames } from '@models/open-games';
 
@@ -12,10 +13,16 @@ import { OpenGames } from '@models/open-games';
 })
 export class GameJoinComponent implements OnInit {
   public gameList: Array<OpenGames>;
+  public currentGame: number;
 
-  constructor(private http: HttpService, public auth: AuthService) {}
+  constructor(
+    private http: HttpService,
+    public auth: AuthService,
+    public game: GameService
+  ) {}
 
   public ngOnInit(): void {
+    this.currentGame = this.game.getGame().gameId;
     this.executeCall();
   }
 
