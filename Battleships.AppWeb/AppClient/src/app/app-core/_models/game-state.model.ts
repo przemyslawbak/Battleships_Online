fbeb4b2573adbf7e788e844c861befd6b58ab097
przemyslawbak <przemyslaw.bak@simple-mail.net>
2020-12-07 09:58:28 +0100

    Changed game-state model;

--- Battleships.AppWeb/AppClient/src/app/app-core/_models/game-state.model.ts
+++ Battleships.AppWeb/AppClient/src/app/app-core/_models/game-state.model.ts
@@ -1,18 +1,13 @@
 import { Player } from '@models/player.model';
 
-export enum GameStage {
-  Deploying,
-  Playing,
-}
-
 export interface GameState {
   gameId: number;
   gameTurnNumber: number;
+  gameTurnPlayer: number;
   gameAi: boolean;
   gameMulti: boolean;
   gameOpen: boolean;
   gameLink: boolean;
-  gameStage: GameStage;
   players: Player[];
   isDeploymentAllowed: boolean;
   isStartAllowed: boolean;
--- Battleships.Models/GameStateModel.cs
+++ Battleships.Models/GameStateModel.cs
@@ -1,20 +1,14 @@
 ﻿namespace Battleships.Models
 {
-    public enum GameStage
-    {
-        Deploying,
-        Playing
-    }
-
     public class GameStateModel
     {
         public int GameId { get; set; }
         public int GameTurnNumber { get; set; }
+        public int GameTurnPlayer { get; set; }
         public bool GameAi { get; set; }
         public bool GameMulti { get; set; }
         public bool GameOpen { get; set; }
         public bool GameLink { get; set; }
-        public GameStage GameStage { get; set; }
         public Player[] Players { get; set; }
         public bool IsDeploymentAllowed { get; set; }
         public bool IsStartAllowed { get; set; }
