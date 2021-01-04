d5182a40daa8fcf3af31437afcbe97e355876c2f
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-04 22:05:21 +0100

    In http-service added 10s connection timeout for get requests;

--- Battleships.AppWeb/AppClient/src/app/app-core/_services/http.service.ts
+++ Battleships.AppWeb/AppClient/src/app/app-core/_services/http.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { catchError, timeout } from 'rxjs/operators';
+import { ModalService } from '@services/modal.service';
 
 @Injectable()
 export class HttpService {
   private result: any;
-  constructor(private http: HttpClient, private spinner: NgxSpinnerService) {}
+  constructor(
+    private http: HttpClient,
+    private spinner: NgxSpinnerService,
+    private modalService: ModalService
+  ) {}
 
   public getData(url: string): Observable<any> {
     this.spinner.show();
@@ -16,6 +22,12 @@ export class HttpService {
   }
 
   private getResult(url: string): Observable<any> {
-    return this.http.get<any>(url);
+    return this.http.get<any>(url).pipe(
+      timeout(10000),
+      catchError((e) => {
+        this.modalService.open('info-modal', 'Connection time out.');
+        return of(null);
+      })
+    );
   }
 }
