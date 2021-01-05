2b808c8768cc8e896e03c19e87744d3cf0a82c60
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-05 09:16:32 +0100

    Updated TokenControllerTests;

--- Battleships.Tests/UnitTests/Controllers/TokenControllerTests.cs
+++ Battleships.Tests/UnitTests/Controllers/TokenControllerTests.cs
@@ -17,6 +17,11 @@ namespace Battleships.Tests.UnitTests.Controllers
 {
     public class TokenControllerTests
     {
+        private readonly string _properEmail = "proper_test_email@gmail.com";
+        private readonly string _wrongEmail = "wrong_test_email@gmail.com";
+        private string _properPassword = "proper_test_password";
+        private readonly string _properToken = "proper_test_token";
+        private readonly string _properName = "proper_test_name";
         private readonly Mock<ITokenService> _tokenServiceMock;
         private readonly Mock<IUserService> _userServiceMock;
         private readonly TokenController _controller;
@@ -25,14 +30,11 @@ namespace Battleships.Tests.UnitTests.Controllers
         private readonly RevokeTokenRequestViewModel _properRevokeTokenModel;
         private readonly TokenResponseViewModel _returnedResponseTokenModel;
         private readonly ExternalLoginInfo _externalLoginInfoInstance;
+        private readonly AppUser _properUser;
 
         public TokenControllerTests()
         {
-            string properEmail = "proper_test_email@gmail.com";
-            string properPassword = "proper_test_password";
-            string properToken = "proper_test_token";
-            string properName = "proper_test_name";
-            Thread.CurrentPrincipal = new TestPrincipalWrapper(new Claim(ClaimTypes.Email, properEmail));
+            Thread.CurrentPrincipal = new TestPrincipalWrapper(new Claim(ClaimTypes.Email, _properEmail));
 
             _externalLoginInfoInstance = new ExternalLoginInfo((ClaimsPrincipal)Thread.CurrentPrincipal, "Facebook", "providers_key", "some_display_name");
             _returnedResponseTokenModel = new TokenResponseViewModel()
@@ -46,24 +48,21 @@ namespace Battleships.Tests.UnitTests.Controllers
             };
             _properRevokeTokenModel = new RevokeTokenRequestViewModel()
             {
-                UserName = properName,
-                Token = properToken,
-                RefreshToken = properToken
+                UserName = _properName,
+                Token = _properToken,
+                RefreshToken = _properToken
             };
             _properRefreshTokenModel = new RefreshTokenRequestViewModel()
             {
-                Email = properEmail,
-                RefreshToken = properToken
+                Email = _properEmail,
+                RefreshToken = _properToken
             };
             _properTokenRequestModel = new TokenRequestViewModel()
             {
-                Email = properEmail,
-                Password = properPassword,
+                Email = _properEmail,
+                Password = _properPassword,
             };
-            AppUser properUser = new AppUser()
-            {
-                Email = properEmail
-            };
+            _properUser = new AppUser() { Email = _properEmail };
             DefaultHttpContext httpContext = new DefaultHttpContext();
             TempDataDictionary tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>())
             {
@@ -74,11 +73,11 @@ namespace Battleships.Tests.UnitTests.Controllers
             _userServiceMock = new Mock<IUserService>();
 
             _userServiceMock.Setup(mock => mock.GetIpAddress(It.IsAny<DefaultHttpContext>())).Returns("127.0.0.1");
-            _userServiceMock.Setup(mock => mock.FindUserByEmail(It.IsAny<string>())).ReturnsAsync(properUser);
+            _userServiceMock.Setup(mock => mock.FindUserByEmail(_properEmail)).ReturnsAsync(_properUser);
+            _userServiceMock.Setup(mock => mock.FindUserByEmail(_wrongEmail)).ReturnsAsync((AppUser)null);
             _userServiceMock.Setup(mock => mock.VerifyUsersPassword(It.IsAny<AppUser>(), It.IsAny<string>())).ReturnsAsync(true);
             _userServiceMock.Setup(mock => mock.GetUserRoleAsync(It.IsAny<AppUser>())).ReturnsAsync("User");
             _userServiceMock.Setup(mock => mock.GetExternalLogin()).ReturnsAsync(_externalLoginInfoInstance);
-            _userServiceMock.Setup(mock => mock.GetRegisterModel(It.IsAny<ExternalLoginInfo>())).Returns(new UserRegisterViewModel());
             _userServiceMock.Setup(mock => mock.CreateNewUserAndAddToDbAsync(It.IsAny<UserRegisterViewModel>())).ReturnsAsync(true);
             _tokenServiceMock.Setup(mock => mock.GenerateTokenResponse(It.IsAny<AppUser>(), It.IsAny<string>(), It.IsAny<string>())).Returns(_returnedResponseTokenModel);
             _tokenServiceMock.Setup(mock => mock.VerifyRefreshToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(true);
@@ -110,9 +109,9 @@ namespace Battleships.Tests.UnitTests.Controllers
         private async Task JsonWebToken_OnUserNotExisting_ReturnsStatusCode409()
         {
             string expectedErrorsResult = "Invalid user details.";
-            _userServiceMock.Setup(mock => mock.FindUserByEmail(It.IsAny<string>())).ReturnsAsync((AppUser)null);
+            TokenRequestViewModel model = new TokenRequestViewModel() { Email = _wrongEmail, Password = _properPassword };
 
-            IActionResult result = await _controller.JsonWebToken(new TokenRequestViewModel());
+            IActionResult result = await _controller.JsonWebToken(model);
             ObjectResult objectResult = result as ObjectResult;
 
             Assert.NotNull(result);
@@ -157,7 +156,21 @@ namespace Battleships.Tests.UnitTests.Controllers
         private async Task RefreshToken_OnUserNotExisting_ReturnsStatusCode409()
         {
             string expectedErrorsResult = "Invalid user details.";
-            _userServiceMock.Setup(mock => mock.FindUserByEmail(It.IsAny<string>())).ReturnsAsync((AppUser)null);
+            RefreshTokenRequestViewModel model = new RefreshTokenRequestViewModel() { Email = _wrongEmail, RefreshToken = _properToken };
+
+            IActionResult result = await _controller.RefreshToken(model);
+            ObjectResult objectResult = result as ObjectResult;
+
+            Assert.NotNull(result);
+            Assert.Equal(StatusCodes.Status409Conflict, objectResult.StatusCode);
+            Assert.Equal(expectedErrorsResult, objectResult.Value);
+        }
+
+        [Fact]
+        private async Task RefreshToken_OnTokenNegativeVerification_ReturnsStatusCode409()
+        {
+            string expectedErrorsResult = "Please log in again.";
+            _tokenServiceMock.Setup(mock => mock.VerifyRefreshToken(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>())).Returns(false);
 
             IActionResult result = await _controller.RefreshToken(new RefreshTokenRequestViewModel());
             ObjectResult objectResult = result as ObjectResult;
@@ -166,8 +179,6 @@ namespace Battleships.Tests.UnitTests.Controllers
             Assert.Equal(StatusCodes.Status409Conflict, objectResult.StatusCode);
             Assert.Equal(expectedErrorsResult, objectResult.Value);
         }
-
-        //todo: test: RefreshToken_OnTokenNegativeVerification_ReturnsStatusCode409()
 
         //RevokeToken
 
@@ -191,14 +202,14 @@ namespace Battleships.Tests.UnitTests.Controllers
             ObjectResult objectResult = result as ObjectResult;
 
             Assert.NotNull(result);
-            Assert.Equal(StatusCodes.Status409Conflict, objectResult.StatusCode);
+            Assert.Equal(StatusCodes.Status500InternalServerError, objectResult.StatusCode);
             Assert.Equal(expectedErrorsResult, objectResult.Value);
         }
 
         //ExternalLoginCallback
 
         [Fact]
-        private async Task ExternalLoginCallback_OnNoRemoteErrorAndInfoReceivedAndUserExisting_ReturnsJsonResult()
+        private async Task ExternalLoginCallback_OnNoRemoteErrorAndInfoReceivedAndUserExisting_ReturnsViewResultWithObject()
         {
             IActionResult result = await _controller.ExternalLoginCallback(null);
             ViewResult resultObject = result as ViewResult;
@@ -247,6 +258,8 @@ namespace Battleships.Tests.UnitTests.Controllers
             string expectedErrorsResult = "Error when creating new user.";
             _userServiceMock.Setup(mock => mock.FindUserByEmail(It.IsAny<string>())).ReturnsAsync((AppUser)null);
             _userServiceMock.Setup(mock => mock.CreateNewUserAndAddToDbAsync(It.IsAny<UserRegisterViewModel>())).ReturnsAsync(false);
+            UserRegisterViewModel model = new UserRegisterViewModel() { Email = _wrongEmail };
+            _userServiceMock.Setup(mock => mock.GetRegisterModel(It.IsAny<ExternalLoginInfo>())).Returns(model);
 
             IActionResult result = await _controller.ExternalLoginCallback(null);
             ObjectResult objectResult = result as ObjectResult;
@@ -256,6 +269,26 @@ namespace Battleships.Tests.UnitTests.Controllers
             Assert.Equal(expectedErrorsResult, objectResult.Value);
         }
 
-        //todo: ExternalLoginCallback_OnUserNotExistingAndCouldCreateNewUser_ReturnView
+        [Fact]
+        private async Task ExternalLoginCallback_OnUserNotExistingAndCouldCreateNewUser_ReturnsViewResultWithObject()
+        {
+            _userServiceMock.Setup(mock => mock.FindUserByEmail(It.IsAny<string>())).ReturnsAsync((AppUser)null);
+            _userServiceMock.Setup(mock => mock.CreateNewUserAndAddToDbAsync(It.IsAny<UserRegisterViewModel>())).ReturnsAsync(true);
+            UserRegisterViewModel model = new UserRegisterViewModel() { Email = _properEmail };
+            _userServiceMock.Setup(mock => mock.GetRegisterModel(It.IsAny<ExternalLoginInfo>())).Returns(model);
+
+            IActionResult result = await _controller.ExternalLoginCallback(null);
+            ViewResult resultObject = result as ViewResult;
+            TokenResponseViewModel resultData = resultObject.ViewData.Model as TokenResponseViewModel;
+
+            Assert.NotNull(result);
+            Assert.IsType<ViewResult>(result);
+            Assert.Equal("foo1", resultData.DisplayName);
+            Assert.Equal("foo2", resultData.Email);
+            Assert.Equal("foo3", resultData.RefreshToken);
+            Assert.Equal("foo4", resultData.Role);
+            Assert.Equal("foo5", resultData.Token);
+            Assert.Equal("foo6", resultData.User);
+        }
     }
 }
