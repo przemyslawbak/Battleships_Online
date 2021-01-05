c693724851ce2ccca236d8394ad4bff75f09a475
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-05 16:05:10 +0100

    Added MemoryAccessTests file;

---
+++ Battleships.Tests/UnitTests/Services/MemoryAccessTests.cs
@@ -0,0 +1,23 @@
+﻿using Battleships.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
+
+namespace Battleships.Tests.UnitTests.Services
+{
+    public class MemoryAccessTests
+    {
+        private readonly Mock<IMemoryCache> _memoryCacheMock;
+        private readonly MemoryAccess _service;
+
+        public MemoryAccessTests()
+        {
+            _memoryCacheMock = new Mock<IMemoryCache>();
+
+            _service = new MemoryAccess(_memoryCacheMock.Object);
+        }
+
+        //GetUserConnectionIdList
+
+        //GetGameList
+    }
+}
