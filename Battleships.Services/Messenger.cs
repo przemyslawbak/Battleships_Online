db1937300c08334573c93b4b65335e9be5358fa9
przemyslawbak <przemyslaw.bak@simple-mail.net>
2021-01-06 19:30:17 +0100

    In MessengerTests added new unit tests;

--- Battleships.Services/Messenger.cs
+++ Battleships.Services/Messenger.cs
@@ -6,13 +6,15 @@ namespace Battleships.Services
 {
     public class Messenger : IMessenger
     {
+        private readonly IUserNameValidator _nameValidator;
         private readonly IMemoryAccess _cache;
         private readonly ISender _sender;
 
-        public Messenger(IMemoryAccess cache, ISender sender)
+        public Messenger(IMemoryAccess cache, ISender sender, IUserNameValidator nameValidator)
         {
             _cache = cache;
             _sender = sender;
+            _nameValidator = nameValidator;
         }
 
         public async Task SendGameStateToUsersInGame(GameStateModel game, IHubCallerClients clients)
@@ -24,7 +26,7 @@ namespace Battleships.Services
 
             foreach (Player player in game.Players)
             {
-                if (IsValidUserName(player.UserName))
+                if (_nameValidator.IsValidUserName(player.UserName))
                 {
                     await _sender.SendGameState(player.UserName, game, clients);
                 }
@@ -35,24 +37,11 @@ namespace Battleships.Services
         {
             foreach (string name in playerNames)
             {
-                if (IsValidUserName(name))
+                if (_nameValidator.IsValidUserName(name))
                 {
                     await _sender.SendChatMesssage(name, message, clients);
                 }
             }
-        }
-
-        private bool IsValidUserName(string name)
-        {
-            if (!string.IsNullOrEmpty(name))
-            {
-                if (name != "COMPUTER")
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
--- Battleships.Tests/UnitTests/Services/MessengerTests.cs
+++ Battleships.Tests/UnitTests/Services/MessengerTests.cs
@@ -1,5 +1,9 @@
-﻿using Battleships.Services;
+﻿using Battleships.Models;
+using Battleships.Services;
+using Microsoft.AspNetCore.SignalR;
 using Moq;
+using System.Threading.Tasks;
+using Xunit;
 
 namespace Battleships.Tests.UnitTests.Services
 {
@@ -7,23 +11,76 @@ namespace Battleships.Tests.UnitTests.Services
     {
         private readonly Mock<ISender> _senderMock;
         private readonly Mock<IMemoryAccess> _cacheMock;
+        private readonly Mock<IUserNameValidator> _nameValidatorMock;
         private readonly Messenger _service;
 
         public MessengerTests()
         {
             _senderMock = new Mock<ISender>();
             _cacheMock = new Mock<IMemoryAccess>();
+            _nameValidatorMock = new Mock<IUserNameValidator>();
 
-            _service = new Messenger(_cacheMock.Object, _senderMock.Object);
+            _service = new Messenger(_cacheMock.Object, _senderMock.Object, _nameValidatorMock.Object);
         }
 
-        //SendGameStateToUsersInGame
-        //todo:
+        [Fact]
+        private async Task SendGameStateToUsersInGame_OnGameSinglePlayerAndValidUserName_CallsRemoveGameFromMemoryOnceAndCallsSendGameState()
+        {
+            _nameValidatorMock.Setup(mock => mock.IsValidUserName(It.IsAny<string>())).Returns(true);
+            Player[] players = new Player[] { new Player() { UserName = "name1", DisplayName = "name2" } };
+            GameStateModel game = new GameStateModel() { GameMulti = false, GameId = 666, Players = players };
+            Mock<IHubCallerClients> clientsMock = new Mock<IHubCallerClients>();
+
+            await _service.SendGameStateToUsersInGame(game, clientsMock.Object);
+
+            _cacheMock.Verify(mock => mock.RemoveGameFromMemory(game.GameId), Times.Once);
+            _senderMock.Verify(mock => mock.SendGameState(It.IsAny<string>(), game, clientsMock.Object), Times.Once);
+        }
+
+        [Fact]
+        private async Task SendGameStateToUsersInGame_OnGameMultiplayerAndNotValidUserName_CallsNeverRemoveGameFromMemoryAndCallsNeverSendGameState()
+        {
+            _nameValidatorMock.Setup(mock => mock.IsValidUserName(It.IsAny<string>())).Returns(false);
+            Player[] players = new Player[] { new Player() { UserName = "", DisplayName = "" } };
+            GameStateModel game = new GameStateModel() { GameMulti = true, GameId = 666, Players = players };
+            Mock<IHubCallerClients> clientsMock = new Mock<IHubCallerClients>();
+
+            await _service.SendGameStateToUsersInGame(game, clientsMock.Object);
+
+            _cacheMock.Verify(mock => mock.RemoveGameFromMemory(game.GameId), Times.Never);
+            _senderMock.Verify(mock => mock.SendGameState(It.IsAny<string>(), game, clientsMock.Object), Times.Never);
+        }
 
         //SendChatMessageToUsersInGame
-        //todo:
+        
+        [Fact]
+        private async Task SendChatMessageToUsersInGame_OnValidUserName_CallsSendChatMesssage()
+        {
+            Player[] players = new Player[] { new Player() { UserName = "name1", DisplayName = "name2" } };
+            GameStateModel game = new GameStateModel() { GameMulti = false, GameId = 666, Players = players };
+            _nameValidatorMock.Setup(mock => mock.IsValidUserName(It.IsAny<string>())).Returns(true);
+            string[] names = new string[] { game.Players[0].UserName };
+            string message = "some_msg";
+            Mock<IHubCallerClients> clientsMock = new Mock<IHubCallerClients>();
 
-        //SendChatMessageToUsersInGame
-        //todo:
+            await _service.SendChatMessageToUsersInGame(message, names, clientsMock.Object);
+
+            _senderMock.Verify(mock => mock.SendChatMesssage(It.IsAny<string>(), message, clientsMock.Object), Times.Once);
+        }
+        
+        [Fact]
+        private async Task SendChatMessageToUsersInGame_OnNotValidUserName_CallsNeverSendChatMesssage()
+        {
+            Player[] players = new Player[] { new Player() { UserName = "", DisplayName = "" } };
+            GameStateModel game = new GameStateModel() { GameMulti = false, GameId = 666, Players = players };
+            _nameValidatorMock.Setup(mock => mock.IsValidUserName(It.IsAny<string>())).Returns(false);
+            string[] names = new string[] { game.Players[0].UserName };
+            string message = "some_msg";
+            Mock<IHubCallerClients> clientsMock = new Mock<IHubCallerClients>();
+
+            await _service.SendChatMessageToUsersInGame(message, names, clientsMock.Object);
+
+            _senderMock.Verify(mock => mock.SendChatMesssage(It.IsAny<string>(), message, clientsMock.Object), Times.Never);
+        }
     }
 }
